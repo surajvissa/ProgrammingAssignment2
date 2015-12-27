665b86f80da08d15bf937eb210e4783932b704d9
Suraj Vissa <surajvissa@gmail.com>
2015-12-27 22:37:28 +0530

    This is Suraj Vissa's submission of Programme Assignment 2, dated 27th December, 2015.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+##A memory-efficient way of potentially laborious computations through caching illustrated.
+##Two functions: 'makeCacheMatrix' & 'cacheSolve' are used to compute the inverse of an input matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##makeCacheMatrix() is a function list which "gets" and "sets" input as well as result values
 
+makeCacheMatrix <- function(x = matrix())  ##An object of type 'matrix' taken as input 
+     
+{
+     m<-NULL              ##Initialization of m used subsequently to store result values
+     
+     set<-function(y) ##Sets new value to x when called, and is cached. M is set to NULL for new input matrix.
+     
+          {
+          x<<-y
+          m<<-NULL
+          }
+     
+     get<-function() x   ## Gets input matrix without requiring any additional input
+     
+     setinverse<-function(solve) m<<- solve  ##Caches result value  
+     
+     getinverse<-function()m  ##Retrieves result without requiring additional input            
+     
+     list(set=set, get=get,setinverse=setinverse,getinverse=getinverse) ##list of functions
+}
+
+##cacheSolve function uses makeCacheMatrix() object as input.
+
+cacheSolve <- function(x,...) 
+     
+{
+     m<-x$getinverse()   # Pulls inverse matrix for input
+     
+     if(!is.null(m))     # If result available, pulls from cache. Else, registers as NULL, and computes inverse.
+     
+          {
+              message("getting cached data")
+              
+              return(m)
+          }
+     
+     matrix<-x$get() 
+     
+     m<-solve(matrix)  # Matrix inverse found using solve() function
+     
+     x$setinverse(m)
+     
+     m
 }
 
 
-## Write a short comment describing this function
+## Specimen Output for input 2X2= c(4,3,3,2)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## a<-makeCacheMatrix(matrix(c(4,3,3,2),2,2,FALSE))
+
+## cacheSolve(a)
+
+##       [,1]  [,2]
+## [1,]   -2     3
+## [2,]    3    -4
